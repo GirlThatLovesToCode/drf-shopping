7728bbc0b9acb05b9f0b01f5340301571fb99513
Michael Herman <hermanmu@gmail.com>
2022-07-03 14:51:13 -0500

    heroku deploy

--- shopping_list/api/views.py
+++ shopping_list/api/views.py
@@ -1,25 +1,107 @@
-from rest_framework import generics
+from drf_spectacular.utils import extend_schema
+from rest_framework import filters, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from shopping_list.api.serializers import ShoppingItemSerializer, ShoppingListSerializer
+from shopping_list.api.pagination import LargerResultsSetPagination
+from shopping_list.api.permissions import (
+    AllShoppingItemsShoppingListMembersOnly,
+    ShoppingItemShoppingListMembersOnly,
+    ShoppingListMembersOnly,
+)
+from shopping_list.api.serializers import (
+    AddMemberSerializer,
+    RemoveMemberSerializer,
+    ShoppingItemSerializer,
+    ShoppingListSerializer,
+)
 from shopping_list.models import ShoppingItem, ShoppingList
 
 
 class ListAddShoppingList(generics.ListCreateAPIView):
-    queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(members=[self.request.user])
+
+    def get_queryset(self):
+        return ShoppingList.objects.filter(members=self.request.user).order_by(
+            "-last_interaction"
+        )
 
 
 class ShoppingListDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
 
+    permission_classes = [ShoppingListMembersOnly]
 
-class AddShoppingItem(generics.CreateAPIView):
-    queryset = ShoppingItem.objects.all()
+
+class ListAddShoppingItem(generics.ListCreateAPIView):
     serializer_class = ShoppingItemSerializer
+    permission_classes = [AllShoppingItemsShoppingListMembersOnly]
+    pagination_class = LargerResultsSetPagination
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields = ["name", "purchased"]  # NEW
+
+    def get_queryset(self):
+        shopping_list = self.kwargs["pk"]
+        queryset = ShoppingItem.objects.filter(shopping_list=shopping_list).order_by(
+            "purchased"
+        )
+
+        return queryset
 
 
 class ShoppingItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ShoppingItem.objects.all()
     serializer_class = ShoppingItemSerializer
-    lookup_url_kwarg = 'item_pk'
+    permission_classes = [ShoppingItemShoppingListMembersOnly]
+    lookup_url_kwarg = "item_pk"
+
+
+class ShoppingListAddMembers(APIView):
+    permission_classes = [ShoppingListMembersOnly]
+
+    # NEW
+    @extend_schema(request=AddMemberSerializer, responses=AddMemberSerializer)
+    def put(self, request, pk, format=None):
+        shopping_list = ShoppingList.objects.get(pk=pk)
+        serializer = AddMemberSerializer(shopping_list, data=request.data)
+        self.check_object_permissions(request, shopping_list)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ShoppingListRemoveMembers(APIView):
+    permission_classes = [ShoppingListMembersOnly]
+
+    # NEW
+    @extend_schema(request=RemoveMemberSerializer, responses=RemoveMemberSerializer)
+    def put(self, request, pk, format=None):
+        shopping_list = ShoppingList.objects.get(pk=pk)
+        serializer = RemoveMemberSerializer(shopping_list, data=request.data)
+        self.check_object_permissions(request, shopping_list)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SearchShoppingItems(generics.ListAPIView):
+    serializer_class = ShoppingItemSerializer
+
+    search_fields = ["name"]
+    filter_backends = (filters.SearchFilter,)
+
+    def get_queryset(self):
+        users_shopping_lists = ShoppingList.objects.filter(members=self.request.user)
+        queryset = ShoppingItem.objects.filter(shopping_list__in=users_shopping_lists)
+
+        return queryset
--- shopping_list/models.py
+++ shopping_list/models.py
@@ -1,11 +1,15 @@
 import uuid
 
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class ShoppingList(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL)  # UPDATED
+    last_interaction = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -15,7 +19,13 @@ class ShoppingItem(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=100)
     purchased = models.BooleanField()
-    shopping_list = models.ForeignKey(ShoppingList, on_delete=models.CASCADE, related_name='shopping_items')
+    shopping_list = models.ForeignKey(
+        ShoppingList, on_delete=models.CASCADE, related_name="shopping_items"
+    )
 
     def __str__(self):
         return self.name
+
+
+class User(AbstractUser):
+    pass
